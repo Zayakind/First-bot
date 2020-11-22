7b8ab80b4ee78f8b20615c8c802ed21224bed39c
Dmitriy <zayakind@gmail.com>
2020-11-22 17:56:39 +0500

    1

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ replica = []
 markup = types.ForceReply(selective=False)
 count = 1
 
-
 @bot.message_handler(content_types=['text'])
 def start_messages(message):
     global count
     if message.text.lower() == 'вопрос':
         if count <= len(test):
             bot.send_message(message.chat.id, f'{test[count]}', reply_markup=markup)
-            replica.append(markup)
+            answer.append()
             count += 1
         else:
             bot.send_message(message.chat.id, f'Вопросы кончились, тест пройден!')
+            count = 0
     if message.text.lower() == 'rpl':
         bot.send_message(message.chat.id, f'{replica}')
 
